198573e48f17cdd45763b04785d12159f2aef195
akshch <akshaych2786@gmail.com>
2024-06-07 16:00:28 +0530

    first commit

---
+++ app/javascript/controllers/task_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="task"
+export default class extends Controller {
+  static targets = ["checkbox"];
+  connect() {}
+
+  toggle() {
+    this.checkboxTarget.closest("form").requestSubmit();
+  }
+}
